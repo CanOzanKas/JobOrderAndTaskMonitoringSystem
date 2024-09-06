96c106acb93d718ac14d0e13d1bfe8809ae6fe17
CanOzanKas <fringee01@gmail.com>
2024-09-06 23:43:31 +0300

    added .gitignore

---
+++ Repository/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JobOrderAndTaskMonitoringSystem.Core.Entities.Notification;
+using JobOrderAndTaskMonitoringSystem.Core.Entities.User;
+using JobOrderAndTaskMonitoringSystem.Core.Entities.JobOrder;
+using Microsoft.EntityFrameworkCore;
+using JobOrderAndTaskMonitoringSystem.Core.Entities.Department;
+
+namespace JobOrderAndTaskMonitoringSystem.Repository.ApplicationDbContext {
+    public class ApplicationDbContext:DbContext {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<JobOrder> JobOrders { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
